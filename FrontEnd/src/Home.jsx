1d308bae93503fbcf86bcd73dd92c3ab6c2c9056
davidbakerrobinson <dbake2@pdx.edu>
2022-11-19 22:15:27 -0800

    Frontend Base

---
+++ FrontEnd/src/Home.jsx
@@ -0,0 +1,42 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, Row, Col} from 'react-bootstrap';
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+
+    return(
+        <div>
+        <div>
+           <h1>Tredagle</h1>      
+        </div>
+        <Container>
+         <Row>
+           <Col xs={3}></Col>
+         <Col xs={6}>
+             <h1></h1>
+           </Col>
+           <Col xs={3}></Col>
+         </Row>
+         <Row>
+           <Col xs={3}>
+           </Col>
+           <Col xs={6}>
+            <Link to='/video_chat'>
+             <Button variant="primary">Enter</Button>
+             </Link>
+           </Col>
+           <Col xs={3}></Col>
+         </Row>
+         <Row>
+           <Col>
+           <p>
+             "The button I wish I never pressed"
+           </p>
+           </Col>
+         </Row>
+        </Container>
+        </div>
+    );
+}
+
+export default Home
