ce2ed43241d89e694c24d6676b71d3bbf4e87ced
Daniel Trang <danieltrangnotes@gmail.com>
2022-11-21 01:48:10 -0800

    added more style to Home page

--- tredagle-app/src/pages/home.jsx
+++ tredagle-app/src/pages/home.jsx
@@ -1,24 +1,22 @@
 import { Link } from "react-router-dom"
 import NavbarProp from "../components/navbar"
+import bg_image from "../assets/triangle-nature.png"
 
 const Home = () => {
     return (
         <>
             <NavbarProp/>
-            <div class="w-screen h-screen bg-indigo-300">
-                <div class="pt-10 pb-5">
-                    <h1 class = "text-center text-3xl font-bold underline">Tredagle landing page</h1>      
+            <div class="w-full h-full flex justify-center">
+                <img src={bg_image} />
+                <div class="fixed top-1/3">
+                    <h1 class = "py-4 text-center text-black text-5xl font-bold underline">Tredagle</h1>      
+                    <div class="flex justify-center py-4">
+                        <Link to="/chatroom">
+                            <button class = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Join Chatroom</button>
+                        </Link>
+                    </div>
+                    <p class="text-center text-indigo-600 italic text-lg">"The button I wish I never pressed"</p>
                 </div>
-                <div class="flex justify-center py-2">
-                <Link to="/chatroom">
-                    <button class = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Join Chatroom
-                    </button>
-                </Link>
-                </div>
-                <p class="text-center">
-                    "The button I wish I never pressed"
-                </p>
             </div>
         </>
     )
