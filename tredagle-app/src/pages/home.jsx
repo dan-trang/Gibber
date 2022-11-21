1bcb06e66d2bc8c74edb029d1a727458e6b374f1
Daniel Trang <danieltrangnotes@gmail.com>
2022-11-21 15:11:07 -0800

    added simple font styling to landing page

--- tredagle-app/src/pages/home.jsx
+++ tredagle-app/src/pages/home.jsx
@@ -8,14 +8,14 @@ const Home = () => {
             <NavbarProp/>
             <div class="flex justify-center">
                 <img class="object-cover w-screen h-screen" src={bg_image} />
-                <div class="fixed my-24 top-1/3">
-                    <h1 class = "py-4 text-center text-black text-5xl font-bold underline">Tredagle</h1>      
+                <div class="fixed p-12 my-6 top-1/3 bg-slate-200 rounded-lg border-4 border-dashed border-black opacity-75 shadow-xl">
+                    <h1 class = "py-4 text-center text-6xl md:text-8xl font-bold font-mono underline">Tredagle</h1>      
                     <div class="flex justify-center py-4">
                         <Link to="/chatroom">
                             <button class = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Join Chatroom</button>
                         </Link>
                     </div>
-                    <p class="text-center text-indigo-600 italic text-lg">"The button I wish I never pressed"</p>
+                    <p class="text-center text-black italic text-lg">"The button I wish I never pressed"</p>
                 </div>
             </div>
         </>
