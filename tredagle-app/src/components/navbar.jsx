0278b42c3f5d21d14f7c52dc3cf340a49f3a85ca
Daniel Trang <danieltrangnotes@gmail.com>
2022-11-22 19:34:07 -0800

    Added to Chatroom page. New video component and styling.

--- tredagle-app/src/components/navbar.jsx
+++ tredagle-app/src/components/navbar.jsx
@@ -1,15 +1,18 @@
 import logo_image from '../assets/t_logo.png'
 import profile_pic from '../assets/chubby_yellow_duck.png'
+import { Link } from 'react-router-dom'
 
 function NavbarProp() {
     return (
         <>
             <nav class="flex items-center px-4 py-2 bg-blue-900/75 shadow-2xl">
-                <div class="cursor-pointer mx-2 flex items-center flex-shrink-0">
-                    <img class="mr-4 h-8 w-8" src={logo_image} alt="" />
-                    <div class="font-mono text-xl font-semibold tracking-wider text-white">Tredagle</div>
-                </div>
-                <ul class="flex flex-row flex-1 justify-end items-center gap-8 uppercase text-xs">
+                <Link to="/">
+                    <div class="cursor-pointer mx-2 flex items-center flex-shrink-0">
+                        <img class="mr-4 h-8 w-8" src={logo_image} alt="" />
+                        <div class="font-mono text-xl font-semibold tracking-wider text-white">Tredagle</div>
+                    </div>
+                </Link>
+                <ul class="hidden md:flex flex-row flex-1 justify-end items-center gap-8 uppercase text-xs">
                     <li class="btn-navbar">18+ Patreon</li>
                     <li class="btn-navbar">OnlyFans</li>
                     <li class="btn-navbar">RawrXXX</li>
@@ -17,6 +20,13 @@ function NavbarProp() {
                         <img class="w-9 h-9 rounded-full"src={profile_pic}/>
                     </li>
                 </ul>
+                <div class="flex md:hidden flex-1 justify-end">
+                    <div class="cursor-pointer space-y-2">
+                        <div class="my-style"></div>
+                        <div class="my-style"></div>
+                        <div class="my-style"></div>
+                    </div>
+                </div>
             </nav>
         </>
     )
---
+++ tredagle-app/src/components/videobox.jsx
@@ -0,0 +1,12 @@
+function VideoBox () {
+
+    return (
+        <>
+        <video class="w-full h-full bg-black border-2 border-stone-900">
+
+        </video>
+        </>
+    )
+} 
+
+export default VideoBox
