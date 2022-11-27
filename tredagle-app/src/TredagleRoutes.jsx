e8c1b5c3eedf2f10e60684dc3f54a8df2fd51483
davidbakerrobinson <91874817+davidbakerrobinson@users.noreply.github.com>
2022-11-27 14:32:45 -0800

    Added socket and start of peerjs

--- tredagle-app/src/TredagleRoutes.jsx
+++ tredagle-app/src/TredagleRoutes.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/home.jsx'
 import Chatroom from './pages/chatroom.jsx'
+import { io } from 'socket.io-client'
+
+const socket = io(`http://${window.location.hostname}:3001`)
 
 export default function TredagleRoutes() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/chatroom" element={<Chatroom/>} />
+        <Route index element={<Home  socket={socket}/> } />
+        <Route path="/chatroom" element={<Chatroom  socket={socket}/> } />
       </Routes>
     </BrowserRouter>
   )
--- tredagle-app/src/pages/chatroom.jsx
+++ tredagle-app/src/pages/chatroom.jsx
@@ -2,8 +2,36 @@ import NavbarProp from "../components/navbar"
 import VideoBox from "../components/videobox"
 import bg_chatroom from "../assets/gradient-1.jpg"
 import { Link } from "react-router-dom"
+import { useState, useEffect, useRef } from "react"
+import { Peer } from 'peerjs'
+const Chatroom = ( {socket} ) => {
+    const localRef = useRef();
+    const remoteRef = useRef();
+    let [remoteID, setremoteID ] = useState("");
+    let [localID, setLocalID ] = useState("");
+    useEffect(()=> {
+        var peer = new Peer();
 
-const Chatroom = () => {
+        peer.on("open", (id)=> {
+            setLocalID(id)
+            socket.emit("userID", {data: id})
+    });   
+
+        var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;   
+      getUserMedia({video: true, audio: true}, (stream)=> {
+         //addVideoStream(document.getElementById("LOCAL"), stream) 
+         localRef.current.srcObject = stream;
+         localRef.current.play();
+      }); // Answer the call with an A/V stream.
+        //localRef.current.srcObject = stream;
+      
+ 
+
+    return () => {
+        socket.off("userID");
+    }
+    }, [])
+    
 
     return(
         <>
@@ -13,8 +41,8 @@ const Chatroom = () => {
                 </div>
                 <img class="object-cover w-screen h-screen" src={bg_chatroom} />
                 <div class="fixed grid grid-cols-2 top-1/4 inset-x-0 mx-auto w-[50rem] h-[18rem] lg:w-[90rem] lg:h-[28rem] gap-x-4 gap-y-1 lg:gap-x-12 lg:gap-y-2">
-                    <VideoBox/>
-                    <VideoBox/>
+                <video id="LOCAL" ref={localRef} class="w-full h-full bg-black border-2 border-stone-900"></video>
+                <video ref={remoteRef} class="w-full h-full bg-black border-2 border-stone-900"></video>
                     <div class="flex justify-center">
                         <Link class="h-fit" to="/">
                             <button class="btn-leave">Leave</button>
