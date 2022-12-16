8993eab6b29a436bc117f3359b1987499b8d4b7b
davidbakerrobinson <91874817+davidbakerrobinson@users.noreply.github.com>
2022-12-16 12:29:18 -0800

    Moved Redis functions to own file

--- be-server/index.js
+++ be-server/index.js
@@ -8,6 +8,12 @@ const Redis = require("ioredis");
 const { v4: uuidv4 } = require('uuid');
 const Origin = "https://capable-toffee-ebaa27.netlify.app";
 //const Origin = "http://127.0.0.1:5173/"
+const {
+    addUserToDB, 
+    checkForUser, 
+    checkIfUserInWaitingRoom, 
+    addUserToActiveSingles
+} = require('./databaseFunctions.js');
 
 require("dotenv").config();
 
@@ -53,50 +59,6 @@ const io = socketio(expressServer, {
     }
 });
 
-/*
-* DESC: Below are functions for communicating with Redis
-* params: err -- error message returned if failed to execute
-*         res -- response from Redis in the form of string (?)
-*         userID -- unique and persistent IDs given to users as they connect with the website
-*         peerID -- unique but temporary IDs to help socket.io connect two remote peers to webcall
-*/
-async function addUserToDB(userID, peerID, socketID) { //timestamp param could go here
-    const result = await client.hset(userID, 'peerID', peerID, 'socketID', socketID, (err, res)=> {
-        if(err) console.log(err);
-        return res;
-    });
-    if(result == 0) console.log("[Updated Existing User] user: " + userID + " / peerID: " + peerID)
-    if(result == 1) console.log("[New User Added] user: " + userID + " / peerID: " + peerID)
-}
-
-async function checkForUser(userID, peerID) {
-    const alreadyJoined = await client.hexists(`${userID}`,"peerID",(err, res)=> {
-        if (err) console.log(err);
-        if(res == 0) console.log(`[User DNE in DB] status #: ${res}`)
-        if(res == 1) console.log(`[User Exists in DB] status #: ${res}`)
-
-        return res;
-    })
-    if(alreadyJoined == 1) {
-            await client.hset(`${userID}`, 'peerID', peerID);
-    }
-    return alreadyJoined;
-}
-
-async function checkIfUserInWaitingRoom(userID) {
-    let existInWaitingRoom = client.smismember('waitingRoom', userID, (err, res)=> {
-        if(err) console.log(err)
-        if(res == 0) console.log(`[User DNE in Waiting Room] status #: ${res}`)
-        if(res == 1) console.log(`[User Exists in Waiting Room] status #: ${res}`)
-        return res
-    })
-    return existInWaitingRoom
-}
-
-async function addUserToActiveSingles(userID) {
-    console.log("Pushing User into Active Singles list")
-    client.rpush('activeSingles', userID);
-}
 
 /* DESC: Socket.io functionalities
 * params: 
