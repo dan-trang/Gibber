72f57c1912246f566291f49f1489b75d023f72d7
davidbakerrobinson <91874817+davidbakerrobinson@users.noreply.github.com>
2022-12-19 11:47:50 -0800

    Check for memory leaks

--- be-server/Users.js
+++ be-server/Users.js
@@ -140,6 +140,7 @@ class Users {
             if(!alreadyInUse) {
                 uniqueID = true;
             }
+            console.log("Generating new UID");
         }
         return UID;
     }
